b6d6532c3af0b62c93a2c42961dc981ccf27d2c6
Nikolay <pythonscript999@gmail.com>
2022-08-06 18:36:18 +0300

    Linked slick slider to js file and initialized values how many slides must be shown, infinite loop of cards etc.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 AOS.init();
+
+$(document).ready(function () {
+    $(".sliders").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true,
+
+        prevArrow:"<button type='button' class='btn-slick-prev slick-prev btn btn-slider'><img src='./images/arrow-left.svg' alt='previous button'></button>",
+        nextArrow:"<button type='button' class='btn-slick-next slick-next btn btn-slider'><img src='./images/arrow-right.svg' alt='next button'></button>"
+    });
+});
